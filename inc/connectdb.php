b7be9b67a3226d7b69be4cc94a6021f1dfa5a79a
Fabian Kurz <fabian@fkurz.net>
2019-11-12 20:38:44 +0100

    connectdb: force character set latin1

--- inc/connectdb.php
+++ inc/connectdb.php
@@ -1,10 +1,11 @@
 <?
 # For a real deployment, you will most likely want to use a custom file here, which is not under version control.
 if (file_exists($_SERVER['DOCUMENT_ROOT']."/inc/connectdb.custom.php")) {
-	include($_SERVER['DOCUMENT_ROOT']."/inc/connectdb.custom.php");
+    include($_SERVER['DOCUMENT_ROOT']."/inc/connectdb.custom.php");
 }
 else {
-	# default values; this works in the docker container
-	$db = mysqli_connect("localhost","lcwo","lcwo", "LCWO") or die ("<h1>Sorry: Could not connect to database.</h1>");
+    # default values; this works in the docker container
+    $db = mysqli_connect("localhost","lcwo","lcwo", "LCWO") or die ("<h1>Sorry: Could not connect to database.</h1>");
+    mysqli_set_charset($db, "latin1");
 }
 ?>
