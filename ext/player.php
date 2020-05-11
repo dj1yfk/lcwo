c5829b8be270fd9dc6f300ab602373505cb2fb00
Fabian Kurz <fabian@fkurz.net>
2020-05-11 20:47:42 +0200

    fix external/embedded player

--- ext/player.php
+++ ext/player.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 <link href="/styleiframe.css" rel="stylesheet" type="text/css">
+<script src="https://lcwo.net/js/jscwlib.js"></script>
 </head>
 <body>  <?# if ($_GET[z]) { echo "onload='playpause1();'";}?>
 <?
@@ -38,8 +39,11 @@ if (!$e) { $e = 20; }
 if (!$s) { $s = 20; }
 if (!is_int($_SESSION[cw_tone])) { $_SESSION[cw_tone] = 600; }
 
-player($t, 4, $s, $e, 0, 1,1,0);
+$_SESSION['vvv']= 0;
+$_SESSION['delay_start']= 0;
+player($t, 1, $s, $e, 0, 1,1,0);
 ?>
+<div id="pv1"></div>
 <a class="footer" target="_blank" href="http://lcwo.net/">Learn CW Online
 - LCWO.net</a> - <a class="footer" target="_blank"  href="http://lcwo.net/text2cw">Text to Morse Converter</a>
 </body>
